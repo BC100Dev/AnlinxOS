f8cd4d3118eb7ea56ab28878468d8c029e67d7c6
BC100Dev <bechris.100@outlook.com>
2025-09-28 22:17:51 +0200

    Refactor ALX64-lib, bye-bye BusyBox

--- Sources/ALX64-Lib/SourceInfo.cmake
+++ Sources/ALX64-Lib/SourceInfo.cmake
@@ -1,13 +1,11 @@
-CollectSources("${PROJECT_MODULE_ROOT}/Shared" SHARED_CODE)
-CollectSources("${PROJECT_MODULE_ROOT}/Lib32" L32_CODE)
-CollectSources("${PROJECT_MODULE_ROOT}/Lib64" L64_CODE)
+CollectSources("${PROJECT_MODULE_ROOT}" ALX64_SRC)
 
-add_library(alx-shared SHARED ${SHARED_CODE})
+add_library(alx-shared SHARED ${ALX64_SRC})
 SetTargetOutputDir(alx-shared "${OUTPUT_DIRECTORY_SYSTEM}/x64")
 SetTargetFilename(alx-shared "alx")
 target_link_libraries(alx-shared PRIVATE os-shared_src-shared)
 
-add_library(alx-static STATIC ${SHARED_CODE})
+add_library(alx-static STATIC ${ALX64_SRC})
 SetTargetOutputDir(alx-static "${OUTPUT_DIRECTORY_SYSTEM}/x64")
 SetTargetFilename(alx-static "alx")
 target_link_libraries(alx-static PRIVATE os-shared_src-static)
